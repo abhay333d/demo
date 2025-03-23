b6701e43f1b6cbd0c5481c9be7531d0fad736497
abhay333d <abhaydandge333@gmail.com>
2025-03-23 11:13:31 +0530

    fixing

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,19 @@ import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
+import path from 'path'
 
 // App Config
 const app = express()
+
+// Define the directory where static files (like AR HTML & JS) are stored
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "HTMLs")));  // Change "public" to your folder name
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "HTMLs", "index.html"));
+});
+
 const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
